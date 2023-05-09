2d61e45ff147ee0b11ab4f1af01f41470f69e823
ArielS76 <arisaetjo@gmail.com>
2023-05-09 19:17:28 -0300

    url actualizacion
    ok

--- src/main/java/com/portfolio/solis/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/solis/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://appa-44c58.web.app/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
